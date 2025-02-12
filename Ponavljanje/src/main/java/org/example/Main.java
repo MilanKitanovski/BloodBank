61407643cd49367f11a8a2ef26ad1acf42fc4138
MilanKitanovski <93132392+MilanKitanovski@users.noreply.github.com>
2025-02-12 15:19:37 +0100

    Create and onnect with database

--- Ponavljanje/src/main/java/org/example/Main.java
+++ Ponavljanje/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(Main.class, args); //pokrece i automatski konfigurise spring boot kontejner
     }
 }
---
+++ Ponavljanje/src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,8 @@
+package org.example.controller;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+@Tag(name = "User Controller", description = "CRUD operacije za korisnike") //dodaje ime grupe i dodaje opis u swaggeru
+
+public class UserController {
+}
